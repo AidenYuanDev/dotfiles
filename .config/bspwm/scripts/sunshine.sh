36de6738d52ce2612f0a6838c25cda449ebe4c36
Aiden Yuan <Aiden.Yuan.Eng@gmail.com>
2025-11-17 01:08:04 +0800

    feat: sunshine light problem

--- .config/bspwm/scripts/sunshine.sh
+++ .config/bspwm/scripts/sunshine.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 xrandr --output DP-2 --scale 0.75x0.675 --brightness 0
-asusctl -n
+asusctl -k off
 feh --bg-scale ~/Pictures/wallpaper/girl_silhouette_paintings_1042284_2560x1600.jpg
--- .config/sxhkd/scripts/lockscreen.sh
+++ .config/sxhkd/scripts/lockscreen.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-
+asusctl -k high
 xrandr --output DP-2 --scale 1x1 --brightness 1
-asusctl -p
 feh --bg-scale ~/Pictures/wallpaper/girl_silhouette_paintings_1042284_2560x1600.jpg
 i3lock -i /home/aiden/Pictures/wallpaper/girl_silhouette_paintings_1042284_2560x1600.jpg
