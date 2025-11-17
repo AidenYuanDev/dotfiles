176cb43c6eb46ef73e6466819f692129fdf0423c
Aiden Yuan <Aiden.Yuan.Eng@gmail.com>
2025-11-17 23:20:30 +0800

    feat: adjust screen rate

--- .config/sxhkd/scripts/lockscreen.sh
+++ .config/sxhkd/scripts/lockscreen.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 asusctl -k high
-xrandr --output DP-2 --scale 1x1 --brightness 1
+xrandr --output DP-2 --mode 2560x1600 --rate 240 --brightness 1
 feh --bg-scale ~/Pictures/wallpaper/girl_glance_wreath_887235_300x168.jpg
-i3lock -i /home/aiden/Pictures/wallpaper/girl_glance_wreath_887235_300x168.jpg
+i3lock -i /home/aiden/Pictures/wallpaper/girl_glance_wreath_887235_300x168.jpg --scale
