d9ba226b28efd82f44497524a46ff206b2bd8f4f
Aiden Yuan <Aiden.Yuan.Eng@gmail.com>
2025-11-17 23:32:43 +0800

    feat: Adapting alacritty to different rate

--- .config/bspwm/scripts/sunshine.sh
+++ .config/bspwm/scripts/sunshine.sh
@@ -2,3 +2,5 @@
 xrandr --output DP-2 --mode 2560x1600 --scale-from 2560x1440 --rate 240 --brightness 0
 asusctl -k off
 feh --bg-scale ~/Pictures/wallpaper/girl_glance_wreath_887235_300x168.jpg
+mv ~/.config/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml.bak
+mv ~/.config/alacritty/alacritty.toml.sunshine ~/.config/alacritty/alacritty.toml
--- .config/sxhkd/scripts/lockscreen.sh
+++ .config/sxhkd/scripts/lockscreen.sh
@@ -2,4 +2,8 @@
 asusctl -k high
 xrandr --output DP-2 --mode 2560x1600 --rate 240 --brightness 1
 feh --bg-scale ~/Pictures/wallpaper/girl_glance_wreath_887235_300x168.jpg
+
+mv ~/.config/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml.sunshine
+mv ~/.config/alacritty/alacritty.toml.bak ~/.config/alacritty/alacritty.toml
+
 i3lock -i /home/aiden/Pictures/wallpaper/girl_glance_wreath_887235_300x168.jpg --scale
